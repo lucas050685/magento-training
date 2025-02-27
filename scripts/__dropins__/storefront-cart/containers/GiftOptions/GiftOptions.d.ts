4c5c73a163b4ee20d12fdfd1f17071da5540dcca
Lucas <58540512+lucas050685@users.noreply.github.com>
2025-02-27 10:55:43 -0300

    Initialize gift-options-pdp

---
+++ scripts/__dropins__/storefront-cart/containers/GiftOptions/GiftOptions.d.ts
@@ -0,0 +1,20 @@
+import { Item } from '../../data/models';
+import { Container } from '@dropins/tools/types/elsie/src/lib';
+import { GiftOptionsViewProps, GiftOptionsDataSourcesProps, GiftFormDataType } from '../../types';
+
+export interface GiftOptionsProps {
+    item: Item;
+    view?: GiftOptionsViewProps;
+    readOnlyFormOrderView: 'primary' | 'secondary';
+    dataSource?: GiftOptionsDataSourcesProps;
+    isEditable?: boolean;
+    initialLoading?: boolean;
+    handleItemsLoading?: (uid: string, state: boolean) => void;
+    handleItemsError?: (uid: string, message?: string) => void;
+    onItemUpdate?: ({ item }: {
+        item: Item;
+    }) => void;
+    onGiftOptionsChange?: (data: GiftFormDataType) => void;
+}
+export declare const GiftOptions: Container<GiftOptionsProps>;
+//# sourceMappingURL=GiftOptions.d.ts.map
